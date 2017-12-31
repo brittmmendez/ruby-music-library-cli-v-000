a95a7b5aa7ef176d2a8017f80ad487b9b684a73c
Brittany Morris <brittmmendez@gmail.com>
2017-12-31 14:16:35 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -86,9 +86,9 @@ require 'pry'
 
     if input >=1 and input <=Song.all.length
       song = Song.all.uniq.sort_by {|obj| obj.name} [input - 1]
+      puts "Playing #{song.name} by #{song.artist.name}" 
     end
-    puts "Playing #{song.name} by #{song.artist.name}" if song
-  
+
   end
 
 end
